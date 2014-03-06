030e9b2e5b1f8407ebfdc29d32eb9161ab01d944
Jordan Hansen <jbhansen84@gmail.com>
2014-03-05 22:18:35 -0700

    Working schedule page.  Ish.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,7 +2,7 @@ class TeamsController < ApplicationController
 	def index
 		@teams = Team.where("season is not null")
 		@matches = Match.all
-		#@matchesrefined = @matches.select {|match| match.home_team.season && match.away_team.season}
+		#@matchesrefined = @matches.select! {|match| match.home_team.season && match.away_team.season}
 	end
 	def show
 		@team = Team.find(params[:id])
