50aed98db8774623769f775178422524e57aa21b
Charlie Croom <ccroom@twitter.com>
2014-03-11 11:04:38 -0700

    fix OpenID store for Heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
+require "openid/fetchers"
+require "openid/store/filesystem"
+
+OpenID.fetcher.ca_file  = "#{Rails.root}/config/certs/cacert.pem"
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :steam, ENV["STEAM_WEB_API_KEY"]
+  provider :steam, ENV["STEAM_WEB_API_KEY"], :store => OpenID::Store::Filesystem.new('./tmp')
 end
