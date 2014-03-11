2254f804213b612bedc53ccfa83c3eda33855f5f
Charlie Croom <charlie.croom@gmail.com>
2014-03-11 10:16:45 -0700

    Fixed bug in teams controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,6 +4,6 @@ class TeamsController < ApplicationController
 	def show
 		# TODO: Is this mess ok?
 		@team = Team.includes({:team_seasons => [:season], :players => [], :away_matches => [:away_team, :home_team], :home_matches => [:away_team, :home_team]}).find(params[:id])
-		@captain_viewing = @current_user.id == @team.captain_id
+		@captain_viewing = @current_user && @current_user.id == @team.captain_id
 	end
 end
