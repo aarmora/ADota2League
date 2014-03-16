e51a41932ad6459f06846b94e24eb1ad552a57a4
Charlie Croom <charlie.croom@gmail.com>
2014-03-16 16:14:48 -0700

    add scores to the schedule page

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -2,13 +2,23 @@ class SeasonsController < ApplicationController
   def index
   	# TODO: this is a hack, set it explicitly to prevent the DB call here
     params[:id] = Season.first.id
-    show    
+    show
   end
   def show
     @seasons = Season.all
     @season = @seasons.detect {|season| season.id == params[:id].to_i}
     @teams = @season.teams # TODO: optimize dual lookup with below?
     @matches = @season.matches.includes(:home_team, :away_team)
+
+    # compute the scores using the pieces we already have so we don't need to re-fetch them again
+    @total_scores = {}
+    @matches.each do |match|
+      @total_scores[match.home_team_id] = @total_scores[match.home_team_id].to_i + match.home_score.to_i
+      @total_scores[match.away_team_id] = @total_scores[match.away_team_id].to_i + match.away_score.to_i
+    end
+
+    @teams.sort_by!{|t| [@total_scores[t.id] * -1, t.teamname]}
+
     @current_tab = 'seasons'
 
     render :action => 'show' # explicitly needed because index calls this method and expects it to render
