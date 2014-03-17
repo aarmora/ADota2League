88b5aebc052e6d2a50a0fd8c8daf27cd1ef345fc
Charlie Croom <charlie.croom@gmail.com>
2014-03-17 09:04:26 -0700

    Fix players controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,10 +1,6 @@
 class PlayersController < ApplicationController
   def index
-  	show
-  end
-  def show
   	@current_tab = "freeagents"
-  	@freeagents = Player.where("freeagentflag = :freeagent", {:freeagent => 1})  
+    @freeagents = Player.where("freeagentflag = :freeagent", {:freeagent => 1})
   end
-
 end
