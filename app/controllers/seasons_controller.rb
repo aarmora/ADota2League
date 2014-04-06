44775cb88b8e18e0aca5fb897d2041ea2dc079c9
Jordan Hansen <jbhansen84@gmail.com>
2014-04-05 22:55:14 -0600

    Invitational season show

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -10,12 +10,27 @@ class SeasonsController < ApplicationController
     @teams = @season.teams # TODO: optimize dual lookup with below?
     @matches = @season.matches.includes(:home_team, :away_team)
     @casters = Player.where(:caster => true)
+    @ths = ["Not quite Gallifrey", "Careless Whisper", "Take off, you hoser", "California Love"]
+    @division = {}
+    #I'm sure there's a way I could do this better
+    @division[0] = @season.team_seasons.where(:division => 1).collect { | ts| ts.team}
+    @division[1] = @season.team_seasons.where(:division => 2).collect { | ts| ts.team}
+    @division[2] = @season.team_seasons.where(:division => 3).collect { | ts| ts.team}
+    @division[3] = @season.team_seasons.where(:division => 4).collect { | ts| ts.team}
 
     # compute the scores using the pieces we already have so we don't need to re-fetch them again
-    @total_scores = {}
-    @matches.each do |match|
-      @total_scores[match.home_team_id] = @total_scores[match.home_team_id].to_i + match.home_score.to_i
-      @total_scores[match.away_team_id] = @total_scores[match.away_team_id].to_i + match.away_score.to_i
+    if params[:id] == "1"
+      @total_scores = {}
+      @matches.each do |match|
+        @total_scores[match.home_team_id] = @total_scores[match.home_team_id].to_i + match.home_score.to_i
+        @total_scores[match.away_team_id] = @total_scores[match.away_team_id].to_i + match.away_score.to_i
+      end
+    elsif params[:id] == "2"
+      @total_scores = {}
+      @matches.each do |match|
+        @total_scores[match.home_team_id] = @total_scores[match.home_team_id].to_i + (match.home_score.to_i == 2 ? 1 : 0)
+        @total_scores[match.away_team_id] = @total_scores[match.away_team_id].to_i + (match.away_score.to_i == 2 ? 1 : 0)
+      end
     end
 
     @teams.sort_by!{|t| [@total_scores[t.id].to_i * -1, t.teamname]}
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ActiveRecord::Base	
-  belongs_to :author, :class_name => "Player"
-  
+  belongs_to :author, :class_name => "Player"  
   attr_accessible :text, :title, :author_id
 end
