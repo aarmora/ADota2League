cc592b3109d2b01ddcbcb17b63e1c1b3aa16ff35
Charlie Croom <charlie.croom@gmail.com>
2014-04-07 17:47:07 -0700

    Fix season caching in controller too?

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -5,37 +5,35 @@ class SeasonsController < ApplicationController
     show
   end
   def show
-    @seasons = Season.all
-    @season = @seasons.detect {|season| season.id == params[:id].to_i}
-    @teams = @season.teams # TODO: optimize dual lookup with below?
-    @matches = @season.matches.includes(:home_team, :away_team)
-    @casters = Player.where(:caster => true)
+    unless fragment_exist?("seasonPage-" + params[:id].to_s)
+      @seasons = Season.all
+      @season = @seasons.detect {|season| season.id == params[:id].to_i}
+      @teams = @season.teams # TODO: optimize dual lookup with below?
+      @matches = @season.matches.includes(:home_team, :away_team)
+      @casters = Player.where(:caster => true)
 
-    # TODO: This should really be on the team season record, change it there and remove this
-    # @division_names = {"1" => "Not quite Gallifrey", "2" => "Careless Whisper", "3" => "Take off, you hoser", "4" => "California Love"]
+      @teams_by_division = @season.team_seasons.where("division IS NOT NULL").group_by(&:division)
 
-    # I'm sure there's a way I could do this better
-    @teams_by_division = @season.team_seasons.where("division IS NOT NULL").group_by(&:division)
-
-    # compute the scores using the pieces we already have so we don't need to re-fetch them again
-    if params[:id] == "1"
-      # don't display the divisions here
-      @teams_by_division = {"" => @teams_by_division.collect {|div, teams| teams}.flatten}
-      @total_scores = {}
-      @matches.each do |match|
-        @total_scores[match.home_team_id] = @total_scores[match.home_team_id].to_i + match.home_score.to_i
-        @total_scores[match.away_team_id] = @total_scores[match.away_team_id].to_i + match.away_score.to_i
+      # compute the scores using the pieces we already have so we don't need to re-fetch them again
+      if params[:id] == "1"
+        # don't display the divisions here
+        @teams_by_division = {"" => @teams_by_division.collect {|div, teams| teams}.flatten}
+        @total_scores = {}
+        @matches.each do |match|
+          @total_scores[match.home_team_id] = @total_scores[match.home_team_id].to_i + match.home_score.to_i
+          @total_scores[match.away_team_id] = @total_scores[match.away_team_id].to_i + match.away_score.to_i
+        end
+      elsif params[:id] == "2"
+        @total_scores = {}
+        @matches.each do |match|
+          @total_scores[match.home_team_id] = @total_scores[match.home_team_id].to_i + (match.home_score.to_i == 2 ? 1 : 0)
+          @total_scores[match.away_team_id] = @total_scores[match.away_team_id].to_i + (match.away_score.to_i == 2 ? 1 : 0)
+        end
       end
-    elsif params[:id] == "2"
-      @total_scores = {}
-      @matches.each do |match|
-        @total_scores[match.home_team_id] = @total_scores[match.home_team_id].to_i + (match.home_score.to_i == 2 ? 1 : 0)
-        @total_scores[match.away_team_id] = @total_scores[match.away_team_id].to_i + (match.away_score.to_i == 2 ? 1 : 0)
-      end
+
+      # @teams.sort_by!{|t| [@total_scores[t.id].to_i * -1, t.teamname]}
+      @matches.sort_by!{|m| m.date ? m.date : Time.now}.reverse
     end
-
-    # @teams.sort_by!{|t| [@total_scores[t.id].to_i * -1, t.teamname]}
-    @matches.sort_by!{|m| m.date ? m.date : Time.now}.reverse
 
     @current_tab = 'seasons'
 
