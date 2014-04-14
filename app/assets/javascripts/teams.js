468ee13f272c62d45228c43e89f186b137f81b4a
Jordan Hansen <jbhansen84@gmail.com>
2014-04-13 23:06:53 -0600

    More front end player add

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-	$('select').select2();
+	//Select2 isn't working and it really is needed to be able to search through the names
+	//$('select').select2();
+	another = $('#addplayers');
+	$('.addplayer').on('change', function(){
+		//Isn't firing for second select
+		another.clone().removeAttr('id').appendTo($(this).closest('.form-horizontal'));
+	});
 });
