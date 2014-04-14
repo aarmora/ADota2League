6cc33fac75cd2cdd314d716716cc60ed313f8cb9
Jordan Hansen <jbhansen84@gmail.com>
2014-04-14 11:17:08 -0600

    More add players

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
 	//Select2 isn't working and it really is needed to be able to search through the names
 	//$('select').select2();
-	another = $('#addplayers');
-	$('.addplayer').on('change', function(){
-		//Isn't firing for second select
+	another = $('#addplayers').clone();
+	$(document).on('change', '.addplayer', function(){
 		another.clone().removeAttr('id').appendTo($(this).closest('.form-horizontal'));
+		$(this).closest('.form-group').find('.button').remove();
+		//weird stuff happening if you select nothing
 	});
 });
