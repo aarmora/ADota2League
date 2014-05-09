667cf36c676baba01c61e0e9d26d1d383849c73a
Jordan Hansen <jbhansen84@gmail.com>
2014-05-09 10:01:23 -0600

    tab fix, redirect for seasons

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -11,36 +11,40 @@ class SeasonsController < ApplicationController
   
       @seasons = Season.all
       @season = @seasons.detect {|season| season.id == params[:id].to_i}
-      @week_num = params[:week].to_i == 0 ? @season.matches.maximum(:week) : params[:week].to_i
+      if @season.nil?
+        redirect_to seasons_path
+      else
+        @week_num = params[:week].to_i == 0 ? @season.matches.maximum(:week) : params[:week].to_i
 
-      # we always need the above, only run all the queries if we need to rebuild the cache or it's an admin
-      if (@current_user && @current_user.is_admin?) || !fragment_exist?("seasonPage-" + params[:id].to_s + "-" + @week_num.to_s)
-        @matches = @season.matches.includes(:home_team, :away_team, :caster).sort_by!{|m| m.date ? m.date : Time.now}.reverse
-        @casters = Player.where(:caster => true)
+        # we always need the above, only run all the queries if we need to rebuild the cache or it's an admin
+        if (@current_user && @current_user.is_admin?) || !fragment_exist?("seasonPage-" + params[:id].to_s + "-" + @week_num.to_s)
+          @matches = @season.matches.includes(:home_team, :away_team, :caster).sort_by!{|m| m.date ? m.date : Time.now}.reverse
+          @casters = Player.where(:caster => true)
 
-        @teams_by_division = @season.team_seasons.includes(:team).group_by {|ts| ts.division.to_s}
+          @teams_by_division = @season.team_seasons.includes(:team).group_by {|ts| ts.division.to_s}
 
-        # compute the scores using the pieces we already have so we don't need to re-fetch them again
-        if params[:id] == "1"
-          # don't display the divisions here
-          @teams_by_division = {"" => @teams_by_division.collect {|div, teams| teams}.flatten}
-          @total_scores = {}
-          @matches.each do |match|
-            @total_scores[match.home_team_id] = @total_scores[match.home_team_id].to_i + match.home_score.to_i
-            @total_scores[match.away_team_id] = @total_scores[match.away_team_id].to_i + match.away_score.to_i
-          end
-        else
-          @total_scores = {}
-          @matches.each do |match|
-            @total_scores[match.home_team_id] = @total_scores[match.home_team_id].to_i + (match.home_score.to_i == 2 ? 1 : 0)
-            @total_scores[match.away_team_id] = @total_scores[match.away_team_id].to_i + (match.away_score.to_i == 2 ? 1 : 0)
+          # compute the scores using the pieces we already have so we don't need to re-fetch them again
+          if params[:id] == "1"
+            # don't display the divisions here
+            @teams_by_division = {"" => @teams_by_division.collect {|div, teams| teams}.flatten}
+            @total_scores = {}
+            @matches.each do |match|
+              @total_scores[match.home_team_id] = @total_scores[match.home_team_id].to_i + match.home_score.to_i
+              @total_scores[match.away_team_id] = @total_scores[match.away_team_id].to_i + match.away_score.to_i
+            end
+          else
+            @total_scores = {}
+            @matches.each do |match|
+              @total_scores[match.home_team_id] = @total_scores[match.home_team_id].to_i + (match.home_score.to_i == 2 ? 1 : 0)
+              @total_scores[match.away_team_id] = @total_scores[match.away_team_id].to_i + (match.away_score.to_i == 2 ? 1 : 0)
+            end
           end
         end
+
+        @current_tab = 'seasons'
+
+        render :action => 'show' # explicitly needed because index calls this method and expects it to render
       end
-
-      @current_tab = 'seasons'
-
-      render :action => 'show' # explicitly needed because index calls this method and expects it to render
   end
 
   def create
