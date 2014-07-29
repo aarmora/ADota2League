86cdc4feadb9a4affc72f01de5b5238f06fe8b8b
Jordan Hansen <jbhansen84@gmail.com>
2014-07-29 07:39:02 -0700

    blog posts, player pages

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,11 +1,8 @@
-var blogApp = angular.module('blogApp', []);
+var blogApp = angular.module('blogApp', ['ngSanitize']);
 
 blogApp.controller('blogController', function ($scope, $http){
 	$http.get('http://pipes.yahoo.com/pipes/pipe.run?_id=0fb06177cb147ba19962305ab987cf3e&_render=json').success(function(posts){
 		$scope.posts = posts.value.items
 		console.log(posts)
-		//$http.get('http://pipes.yahoo.com/pipes/pipe.run?_id=c274017e2a5f42258d45e64dbd3d8caf&_render=json').success(function(comments){
-		//	$scope.comments
-		//});
 	});
 });
