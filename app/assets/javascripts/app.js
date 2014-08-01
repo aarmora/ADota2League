2cd430cca2794c72c6b56a574286c9123ba3dff6
Jordan Hansen <jbhansen84@gmail.com>
2014-07-31 18:46:53 -0600

    more

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,4 @@
-var blogApp = angular.module('blogApp', []);
+var blogApp = angular.module('blogApp', ['ngSanitize']);
 
 blogApp.controller('blogController', function ($scope, $http){
 	$http.get('http://pipes.yahoo.com/pipes/pipe.run?_id=0fb06177cb147ba19962305ab987cf3e&_render=json').success(function(posts){
