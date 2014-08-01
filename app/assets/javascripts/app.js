c2f8766e07e4e3d1762dcc25f1953db5b8fb73cc
Jordan Hansen <jbhansen84@gmail.com>
2014-07-31 18:52:49 -0600

    fixing for minified angular

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,8 +1,9 @@
 var blogApp = angular.module('blogApp', ['ngSanitize']);
 
-blogApp.controller('blogController', function ($scope, $http){
+blogApp
+.controller('blogController', ['$scope', '$http', function ($scope, $http){
 	$http.get('http://pipes.yahoo.com/pipes/pipe.run?_id=0fb06177cb147ba19962305ab987cf3e&_render=json').success(function(posts){
 		$scope.posts = posts.value.items
 		console.log(posts)
 	});
-});
+}]);
