ae66538542e74e9fefcea3c2b663208d8dc2c4df
Jordan Hansen <jbhansen84@gmail.com>
2014-08-03 12:04:57 -0600

    fix FA

--- app/models/player.rb
+++ app/models/player.rb
@@ -13,9 +13,6 @@ class Player < ActiveRecord::Base
   has_many :endorsers, :through => :endorsements
   has_many :outgoing_endorsements, :class_name => "PlayerVote", :foreign_key => "endorser_id", :dependent => :delete_all
 
-  has_many :player_comments, :class_name => "PlayerComment", :foreign_key => "recipient_id", :dependent => :delete_all
-  has_many :commenters, :through => :player_comments
-  has_many :outgoing_comments, :class_name => "PlayerComment", :foreign_key => "commenter_id", :dependent => :delete_all
 
   attr_accessible :name, :bio, :email, :time_zone, :freeagentflag, :role, :mmr, :hours_played, :as => [:default, :caster, :admin]
   attr_accessible :twitch, :region, :as => [:admin, :caster]
