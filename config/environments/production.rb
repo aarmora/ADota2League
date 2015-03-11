3ac68f2881ae1fd3d7c92114afc53cdf2e379ee4
Charlie Croom <ccroom@twitter.com>
2015-03-11 11:50:07 -0700

    Compress JS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,7 @@ Ad2l::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
+  config.assets.js_compressor = :uglifier
 
   # Generate digests for assets URLs
   config.assets.digest = true
