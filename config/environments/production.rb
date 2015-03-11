533f2b2acac97e819fa4b7b1788e650417254221
Charlie Croom <ccroom@twitter.com>
2015-03-11 16:19:27 -0700

    Oink to prod only

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,10 +15,10 @@ Ad2l::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
+  config.assets.js_compressor = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
-  config.assets.js_compressor = :uglifier
 
   # Generate digests for assets URLs
   config.assets.digest = true
--- config/initializers/oink.rb
+++ config/initializers/oink.rb
@@ -1,5 +1,4 @@
-require 'oink'
-
 if Rails.env.production?
+  require 'oink'
 	Rails.application.middleware.use(Oink::Middleware, :logger => Hodel3000CompliantLogger.new(STDOUT))
 end
