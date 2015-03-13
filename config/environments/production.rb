c1e9f65064eb4fdedc5bae40e566b1c8efe3f31d
Jordan <jbhansen84@gmail.com>
2015-03-13 13:36:08 -0600

    Add ad2l to exception notification

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Ad2l::Application.configure do
     :email => {
       :email_prefix => "[AD2L Site] ",
       :sender_address => %{"AD2L Bug Notifier" <amateurdota2league@gmail.com>},
-      :exception_recipients => %w{charlie.croom@gmail.com, jbhansen84@gmail.com},
+      :exception_recipients => %w{charlie.croom@gmail.com, jbhansen84@gmail.com, amateurdota2league@gmail.com},
       :normalize_subject => true
     }
     #,
